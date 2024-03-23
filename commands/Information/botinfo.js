f3efa26a05e69a06807d4e7e8699aae62220d814
interaction.run <85559340+QrIvan@users.noreply.github.com>
2024-03-23 11:45:42 -0400

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -9,21 +9,18 @@ module.exports = {
     examples: ["botinfo"],
     run: async (client, message, args) => {
 
-        // Obtén la versión actual del bot y la lista de comandos
-        const botVersion = '**v1.0.5**'; // Reemplaza esto con la versión real de tu bot
-        const devInfo = 'QrIvan#0105\nMr. Tom Kinane#3153';
+        const botVersion = '**v1.0.5**';
+        const devInfo = '[QrIvan#0105](https://github.com/QrIvan)';
 
-        // Construye un mensaje embed con la información recopilada
         const botInfoEmbed = new MessageEmbed()
             .setColor('RANDOM')
             .setTitle('`sytem@bot/information`\n📂 **|** Bot Information')
-            .addField('<:2020_devlogo:1183420447291879574> | Developers', devInfo, true)
-            .addField('<:Bot_Logo:1183460213496484053> | Bot Version', botVersion, true)
-            .addField('<:users_logo:1183420445635133460> | Members', client.users.cache.size.toString(), true)
-            .addField('<a:ZLogo3TTM:1183420441897996298> | Activity time', formatUptime(process.uptime()), true)
+            .addField(' | Developers', devInfo, true)
+            .addField(' | Bot Version', botVersion, true)
+            .addField(' | Members', client.users.cache.size.toString(), true)
+            .addField(' | Activity time', formatUptime(process.uptime()), true)
             .setFooter('Thanks for using our bot!');
 
-        // Envía el mensaje embed al canal donde se invocó el comando
         message.reply({ embeds: [botInfoEmbed] });
     },
 };
