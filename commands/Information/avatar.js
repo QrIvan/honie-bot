c1d4b05a3cdafcf16c277e9f42891ec98ec43e20
interaction.run <85559340+QrIvan@users.noreply.github.com>
2024-03-23 11:44:26 -0400

    Update avatar.js

--- commands/Information/avatar.js
+++ commands/Information/avatar.js
@@ -8,20 +8,15 @@ module.exports = {
     examples: ["avatar @QrIvan", "av 828991790324514887"],
     usage: 'avatar  @Member|ID',
     run: async (client, message, args) => {
-        // Obtén al usuario mencionado o al autor del mensaje
         const user = message.mentions.users.first() || message.author;
-
-        // Obtén la URL del avatar del usuario
         const avatarURL = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
 
-        // Crea un mensaje embed con el avatar
         const avatarEmbed = new MessageEmbed()
             .setTitle(`Avatar of ${user.tag}`)
             .setColor('#0099ff')
             .setImage(avatarURL)
             .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
 
-        // Envía el mensaje embed
         message.reply({ embeds: [avatarEmbed] });
     },
 };
