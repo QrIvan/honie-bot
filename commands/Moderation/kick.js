33bb84e44f970a64b5eda4d208a491217d6d149e
interaction.run <85559340+QrIvan@users.noreply.github.com>
2024-03-23 11:39:56 -0400

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -8,76 +8,65 @@ module.exports = {
     usage: 'kick @user|ID [reason]',
     examples: ["kick @QrIvan Test", "kick 828991790324514887 Test"],
     run: async (client, message, args) => {
-        // Check user permissions
         if (!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
             const noPermissionEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/perms/kick`\n<:Moderator_Logo:1183460215782391828> **KICK** | Permission Denied')
+                .setTitle('**KICK** | Permission Denied')
                 .setDescription('You do not have permissions to use this command.');
             return message.reply({ embeds: [noPermissionEmbed] });
         }
 
-        // Check if a user is mentioned or provided by ID
         const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
         if (!target) {
             const noUserMentionedEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/kick`\n<:Moderator_Logo:1183460215782391828> **KICK** | Missing User Mention')
+                .setTitle('**KICK** | Missing User Mention')
                 .setDescription('Valid use: `h/kick [@user|user_id] [reason]`.');
             return message.reply({ embeds: [noUserMentionedEmbed] });
         }
 
-        // Check if the bot has permissions to kick members
         if (!message.guild.me.permissions.has('KICK_MEMBERS')) {
             const botNoPermissionEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/kick`\n<:Moderator_Logo:1183460215782391828> **KICK** | Bot Permission Denied')
+                .setTitle('**KICK** | Bot Permission Denied')
                 .setDescription('The bot does not have permissions to kick members.');
             return message.reply({ embeds: [botNoPermissionEmbed] });
         }
 
-        // Check if the member has a role higher than the bot's role
         if (target.roles.highest.comparePositionTo(message.guild.me.roles.highest) >= 0) {
             const roleTooHighEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/kick`\n<:Moderator_Logo:1183460215782391828> **KICK** | Role Hierarchy Issue')
+                .setTitle('**KICK** | Role Hierarchy Issue')
                 .setDescription('I cannot kick this user due to their roles.');
             return message.reply({ embeds: [roleTooHighEmbed] });
         }
 
-        // Check if a reason for the kick is provided
         const reason = args.slice(1).join(' ') || 'Force-Kick';
-
-        // Perform the kick
         try {
             await target.kick(reason);
 
-            // Create an embed message for the moderation log channel
             const modLogEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/modlog/kick`\n<a:uh_stafflogo:1183420465902006273> Honie Studios | Kick Log')
+                .setTitle('Honie Studios | Kick Log')
                 .setDescription(`**Kicked User:** ${target.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
 
-            // Send the log to the moderation log channel
-            const LOGS_CHANNEL = '1163269047283101716'; // Importa LOGS_CHANNEL 
+            const LOGS_CHANNEL = 'LOGS_CHANNEL'; // Importa LOGS_CHANNEL 
             const modLogChannel = message.guild.channels.cache.get(LOGS_CHANNEL);
 
             if (modLogChannel && modLogChannel.type === 'text') {
                 modLogChannel.send({ embeds: [modLogEmbed] });
             }
 
-            // Create an embed message to send to the kicked user
             const kickEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/kicked`\n<a:uh_stafflogo:1183420465902006273> Honie Server | Left the Server')
+                .setTitle('Honie Server | Left the Server')
                 .setDescription(`You have been kicked from the server **${message.guild.name}**`)
                 .addField('Moderator', message.author.tag)
                 .addField('Reason', reason)
-                .addField('Invitation', '[Click Here](https://rebrand.ly/honiediscord)')
+                .addField('Invitation', '[Click Here](https://rebrand.ly/folkdiscord)')
                 .setFooter('If you wish to return to the server, here is an invitation.');
 
-            // Send a private message to the kicked user with the embed
             target.user.send({ embeds: [kickEmbed] })
                 .catch(error => {
                     console.error(`[CONSOLE ERROR] Error sending a private message to ${target.user.tag}: ${error}`);
@@ -88,7 +77,7 @@ module.exports = {
             console.error(error);
             const kickErrorEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/kick`\n<:Moderator_Logo:1183460215782391828> **KICK** | Error Kicking User')
+                .setTitle('**KICK** | Error Kicking User')
                 .setDescription('An error occurred while trying to kick the user.');
             return message.reply({ embeds: [kickErrorEmbed] });
         }
