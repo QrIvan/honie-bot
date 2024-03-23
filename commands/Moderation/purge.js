dc9fe7e6136c4318b13f760bbf53b965c1fa39d9
interaction.run <85559340+QrIvan@users.noreply.github.com>
2024-03-23 11:28:43 -0400

    Update purge.js

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -8,11 +8,10 @@ module.exports = {
     examples: ["purge 1", "clear 100"],
     category: 'Moderation',
     run: async (client, message, args, prefix) => {
-        // Check user permissions
         if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
             const noPermissionEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/perms/purge`\n<:Moderator_Logo:1183460215782391828> **PURGE** | Permission Denied')
+                .setTitle('**PURGE** | Permission Denied')
                 .setDescription('You do not have permissions to use this command.');
             return message.reply({ embeds: [noPermissionEmbed] });
         }
@@ -20,7 +19,7 @@ module.exports = {
         if (isNaN(args[0])) {
             const invalidAmountEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/purge`\n<:Moderator_Logo:1183460215782391828> **PURGE** | Invalid Amount')
+                .setTitle('**PURGE** | Invalid Amount')
                 .setDescription('Valid use: `h/purge [amount]`.');
             return message.reply({ embeds: [invalidAmountEmbed] });
         }
@@ -30,41 +29,34 @@ module.exports = {
         if (amount < 1 || amount > 100) {
             const amountOutOfRangeEmbed = new MessageEmbed()
                 .setColor('#ff0000')
-                .setTitle('`sytem@user/error/purge`\n<:Moderator_Logo:1183460215782391828> **PURGE** | Amount Out of Range')
+                .setTitle('**PURGE** | Amount Out of Range')
                 .setDescription('Valid use: `h/purge [amount] (Max. 100)`..');
             return message.reply({ embeds: [amountOutOfRangeEmbed] });
         }
 
-        // Perform message purge
         message.channel.bulkDelete(amount)
             .then(messages => {
                 const deletedCount = messages.size;
 
-                // Get the log channel from the config
-                const canalDeRegistrosId = '1183447592395350128'; // Reemplaza con el ID del canal de registros
+                const canalDeRegistrosId = 'CHANNEL_LOG_ID'; // Reemplaza con el ID del canal de registros
 
-                // Get the log channel
                 const logChannel = message.guild.channels.cache.get(canalDeRegistrosId);
 
                 if (logChannel && logChannel.type === 'text') {
-                    // Send a log message in the log channel
                     const logEmbed = new MessageEmbed()
                         .setColor('#00ff00')
-                        .setTitle('`sytem@user/modlog/purge`\n🗑️ Honie Bot | Messages Deleted')
+                        .setTitle('🗑️ Honie Bot | Messages Deleted')
                         .setDescription(`**Deleted Message Count:** ${deletedCount}\n**Moderator:** ${message.author.tag}`)
                         .setTimestamp();
 
                     logChannel.send({ embeds: [logEmbed] });
                 }
-
-                // Send a confirmation message
                 const confirmationEmbed = new MessageEmbed()
                     .setColor('#00ff00')
-                    .setDescription(`<:utility12:1183420388580012094> **PURGE** | Deleted \`${deletedCount}/${amount}\` messages.`);
+                    .setDescription(`**PURGE** | Deleted \`${deletedCount}/${amount}\` messages.`);
 
                 message.channel.send({ embeds: [confirmationEmbed] })
                     .then(msg => {
-                        // Delete the confirmation message after 10 seconds
                         setTimeout(() => {
                             msg.delete().catch(() => null);
                         }, 10000);
@@ -75,7 +67,7 @@ module.exports = {
                 console.error(error);
                 const purgeErrorEmbed = new MessageEmbed()
                     .setColor('#ff0000')
-                    .setTitle('`sytem@user/error/purge`\n<:Moderator_Logo:1183460215782391828> **Error** | Error Deleting Messages')
+                    .setTitle('**Error** | Error Deleting Messages')
                     .setDescription('You can only bulk delete messages that are under 14 days old.');
                 message.reply({ embeds: [purgeErrorEmbed] });
             });
