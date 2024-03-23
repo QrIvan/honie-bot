72b65020e51622338241a0097aa1f4367bf589ec
interaction.run <85559340+QrIvan@users.noreply.github.com>
2024-03-23 10:57:03 -0400

    Update avatar.js

--- slash_commands/Information/avatar.js
+++ slash_commands/Information/avatar.js
@@ -10,21 +10,15 @@ module.exports = {
                 .setDescription('👥 The user whose avatar you want to see.')
         ),
         async run(client, interaction) {
-        // Get the mentioned user or the author of the interaction
         const user = interaction.options.getUser('user') || interaction.user;
 
-        // Get the URL of the user's avatar
         const avatarURL = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
-
-        // Create an embed message with the avatar
         const avatarEmbed = new MessageEmbed()
             .setTitle(`Avatar of ${user.tag}`)
             .setColor('#0099ff')
             .setImage(avatarURL)
             .setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL())
             .setTimestamp();
-
-        // Send the embed message as a response to the interaction
         await interaction.reply({ embeds: [avatarEmbed] });
     },
 };
